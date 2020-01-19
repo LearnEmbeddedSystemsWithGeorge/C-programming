b010a0703effd9b8c4df33ea516ff62585c2aae0
LearnEmbeddedSystemsWithGeorge <60072960+LearnEmbeddedSystemsWithGeorge@users.noreply.github.com>
2020-01-19 20:14:46 +0200

    Add files via upload

---
+++ HelloWorld/Lesson_01.c
@@ -0,0 +1,16 @@
+/*
+ * Lesson_01.c
+ *
+ *  Created on: Jan 13, 2020
+ *      Author: preli
+ */
+
+/* Standard input output library */
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Hello World!\n");
+
+	return 0;
+}
