d4ab14152bb8c41bdf12f43f507d1e8011cc9381
LearnEmbeddedSystemsWithGeorge <60072960+LearnEmbeddedSystemsWithGeorge@users.noreply.github.com>
2020-01-28 23:26:21 +0200

    Add files via upload

---
+++ Lesson_05/lesson_05.c
@@ -0,0 +1,13 @@
+/* C Type Qualifiers
+ * lesson_05.c
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	const short int constVariable = 20;
+
+	printf("The value of constant variable is: %d\n", constVariable);
+
+	return 0;
+}
